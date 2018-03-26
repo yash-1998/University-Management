b9b3a0ab1c804f7a6357f0829f23b81a6acedd38
yash-1998 <eternalblizzard1998@gmail.com>
2018-03-26 10:44:17 +0530

    Small Changes

--- login.php
+++ login.php
@@ -39,7 +39,7 @@
           <a class="btn btn-primary btn-block" href="index.html">Login</a>
         </form>
         <div class="text-center">
-          <a class="d-block small mt-3" href="register.html">Register an Account</a>
+          <a class="d-block small mt-3" href="register.php">Register an Account</a>
           <a class="d-block small" href="forgot-password.html">Forgot Password?</a>
         </div>
       </div>
