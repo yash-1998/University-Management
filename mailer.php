3eb11bcf3fa1a30a8f414450f49e612e3c908d8f
yash-1998 <eternalblizzard1998@gmail.com>
2018-03-27 20:46:10 +0530

    Register OTP added

---
+++ mailer.php
@@ -0,0 +1,43 @@
+<?php session_start();
+// Import PHPMailer classes into the global namespace
+// These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+//Load Composer's autoloader
+require 'vendor/autoload.php';
+
+$mail = new PHPMailer(true);                              // Passing `true` enables exceptions
+try {
+    //Server settings                              // Enable verbose debug output
+    $mail->isSMTP();                                      // Set mailer to use SMTP
+    $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+    $mail->SMTPAuth = true;                               // Enable SMTP authentication
+    $mail->Username = 'yashcoderiiita@gmail.com';                 // SMTP username
+    $mail->Password = 'demonchild1998';                           // SMTP password
+    $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+    $mail->Port = 587;                                    // TCP port to connect to
+
+    //Recipients
+    $mail->setFrom('yashcoderiiita@gmail.com', 'Yash');
+    $mail->addAddress('mridulgupta11944@gmail.com', 'Mridul');     // Add a recipient
+    //$mail->addAddress('ellen@example.com');               // Name is optional
+    //$mail->addReplyTo('info@example.com', 'Information');
+    //$mail->addCC('cc@example.com');
+    //$mail->addBCC('bcc@example.com');
+
+    //Attachments
+    //$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+    //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+
+    //Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = 'OTP';
+    $mail->Body    = 'Enter this OTP to register successfully. '.$_SESSION['OTP'];
+    //$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo 'Message could not be sent. Mailer Error: ', $mail->ErrorInfo;
+}
--- register.php
+++ register.php
@@ -57,11 +57,18 @@
               </div>
             </div>
           </div>
-          <button class="btn btn-primary btn-block" name="registerbtn">Register</button>
+          <button class="btn btn-primary btn-block" name="submitbtn">Submit</button>
+        </form>
+        <form action="" method="POST">
+                <div class="col-md-6" id="otp">
+                <label for="exampleConfirmPassword">Enter Verification Code</label>
+                <input name="rotp" class="form-control" id="exampleConfirmPassword" type="number" placeholder="Verification Code">
+                <button class="btn btn-primary btn-block" name="registerbtn">Register</button>
+              </div>
+              
         </form>
         <div class="text-center">
           <a class="d-block small mt-3" href="login.php">Login Page</a>
-          <a class="d-block small" href="forgot-password.php">Forgot Password?</a>
         </div>
       </div>
     </div>
@@ -76,8 +83,18 @@
 </html>
 
 <?php
-
-    if(isset($_REQUEST['registerbtn']))
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\Exception;
+    require 'vendor/autoload.php';
+    $flag = true;
+    $rfirstname = "";
+    $rlastname = "";
+    $rusername = "";
+    $rpassword = "";
+    $reamil = "";
+    $otp = 1000000;
+    
+    if(isset($_REQUEST['submitbtn']))
     {
             if(isset($_REQUEST['rfirstname']))
             {
@@ -132,10 +149,87 @@
             {
                   $error = "Error! some of  the required fields are empty!!";
                   echo "<script type='text/javascript'>alert(\"$error\");</script>";
+                  $flag = false;
             }
             if($rpassword != $rconfirmpassword)
             {
                   $error = "Error! Both passwords do not match";
                   echo "<script type='text/javascript'>alert(\"$error\");</script>";
+                  $flag = false;
+            }
+
+            $otp = rand()%10000;
+            $_SESSION['checkotp']=$otp;
+            if($flag == true)
+            {
+                if(rmail($otp,$remail))
+                {
+                    $error = "Please enter the verificaiton code sent to your mail for registering";
+                    echo "<script type='text/javascript'>alert(\"$error\");</script>";
+                }
+                else
+                {
+                    $error = "Invalid email";
+                    echo "<script type='text/javascript'>alert(\"$error\");</script>";
+                }
             }
     }
+
+    function rmail($otp,$remail)
+    {
+            $mail = new PHPMailer(true);                              // Passing `true` enables exceptions
+            try {
+                //Server settings                              // Enable verbose debug output
+                $mail->isSMTP();                                      // Set mailer to use SMTP
+                $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+                $mail->SMTPAuth = true;                               // Enable SMTP authentication
+                $mail->Username = 'yashcoderiiita@gmail.com';                 // SMTP username
+                $mail->Password = 'eternalblizzard1998';                           // SMTP password
+                $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+                $mail->Port = 587;                                    // TCP port to connect to
+
+                //Recipients
+                $mail->setFrom('yashcoderiiita@gmail.com', 'Yash');
+                $mail->addAddress($remail);     // Add a recipient
+                //$mail->addAddress('ellen@example.com');               // Name is optional
+                //$mail->addReplyTo('info@example.com', 'Information');
+                //$mail->addCC('cc@example.com');
+                //$mail->addBCC('bcc@example.com');
+
+                //Attachments
+                //$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+                //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+
+                //Content
+                $mail->isHTML(true);                                  // Set email format to HTML
+                $mail->Subject = 'OTP';
+                $mail->Body    = 'Enter this OTP to register successfully. '.$otp;
+                //$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+                $mail->send();
+                  return true;
+            } catch (Exception $e) {
+                return false;
+            }
+    }
+    if(isset($_REQUEST['registerbtn']))
+    {
+        if(isset($_REQUEST['rotp']))
+        {
+            $enterotp = $_REQUEST['rotp'];
+            
+            if($enterotp != $_SESSION['checkotp'])
+            {
+                $error = "Wrong Verification Code";
+                echo "<script type='text/javascript'>alert(\"$error\");</script>";
+            }
+            else
+            {
+                $error = "Susscessful";
+                echo "<script type='text/javascript'>alert(\"$error\");</script>";
+            }
+        }
+    }
+
+
+?>
