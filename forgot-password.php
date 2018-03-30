6bd7cbce4e59e86b23de1c601e8bf6d02b4717b0
siddhant81 <iit2016069@iiita.ac.in>
2018-03-30 23:40:11 +0530

    sidthe1

--- forgot-password.php
+++ forgot-password.php
@@ -22,7 +22,7 @@
       <div class="card-header">Reset Password</div>
       <div class="card-body">
         <div class="text-center mt-4 mb-5">
-          <h4>Forgot your password?</h4>
+          <h4>Forgot your passwordkjwnefkjnwefkj?</h4>
           <p>Enter your email address and we will send you instructions on how to reset your password.</p>
         </div>
         <form>
